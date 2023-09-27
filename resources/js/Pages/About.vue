4402a5221efd0662c64fa3b9c4fb50f1154db3e6
joren97x <babamyloves2002@gmail.com>
2023-09-27 18:51:45 +0630

    setup environment laravel/vue/inertia

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+    import { Link } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <v-container>
+        <p class="text-h1 text-center">About page</p>
+        <Link href="/">Home</Link>
+    </v-container>
+</template>
---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+</script>
+<template>
+    <v-container>
+        <p class="text-center text-h1">Hello giatay!</p>
+        <Link href="/about">About</Link>
+    </v-container>
+</template>
